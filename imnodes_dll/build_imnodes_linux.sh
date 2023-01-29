855602a121e0a8ad25de01dbca4a67445d6e6651
vaiorabbit <vaiorabbit>
2023-01-29 22:54:33 +0900

    Update workflow

--- imnodes_dll/build_imnodes_linux.sh
+++ imnodes_dll/build_imnodes_linux.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
+cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D CMAKE_CXX_COMPILER=clang++ ../
 make
 arch=`uname -m`
 mv imnodes.so imnodes.${arch}.so
