14251be3df5cdaa9c6bbe66dc14079e68f14c6b6
vaiorabbit <vaiorabbit>
2023-01-29 22:48:09 +0900

    Update workflow

--- imgui_dll/build_imgui_linux_cross.sh
+++ imgui_dll/build_imgui_linux_cross.sh
@@ -1,6 +1,6 @@
 mkdir -p build
 cd build
-cmake -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
+cmake -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON -D CMAKE_C_FLAGS=-isystem\ /usr/aarch64-linux-gnu/include -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER_TARGET=aarch64-linux-gnu -D CMAKE_SYSTEM_PROCESSOR=ARM -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
 make
 export ARCH=aarch64
 mv imgui.so imgui.${ARCH}.so
