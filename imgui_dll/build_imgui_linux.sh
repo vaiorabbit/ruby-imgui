096e2634821a450a12509d1e4e896746417b9039
vaiorabbit <vaiorabbit>
2024-01-12 14:11:38 +0000

    commit by vaiorabbit via GitHub Actions

--- imgui_dll/build_imgui_linux.sh
+++ imgui_dll/build_imgui_linux.sh
@@ -3,5 +3,4 @@ cd build
 cmake -D CMAKE_BUILD_TYPE=Release -D BUILD_SHARED_LIBS=ON -D CMAKE_CXX_COMPILER=clang++ ../
 make
 arch=`uname -m`
-mv imgui.so imgui.${arch}.so
-cp *.so ../../lib
+cp imgui.so ../../lib/imgui.${arch}.so
