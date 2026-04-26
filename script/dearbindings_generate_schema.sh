99f583d5a730639887b8d4aa583fb7d3cd24d953
vaiorabbit <vaiorabbit>
2026-04-26 13:56:54 +0900

    Remove unused scripts

--- script/dearbindings_generate_schema.sh
+++
@@ -1,4 +0,0 @@
-pushd ../third_party/
-python3.11 dear_bindings/dear_bindings.py -t ./dear_bindings/src/templates -o ../generator/dearbindings ../imgui_dll/cimgui/imgui/imgui.h
-rm ../generator/dearbindings.cpp ../generator/dearbindings.h
-popd
